43ecb0b965e82b1a6a43a458cdbfc9b1c526536f
jomi087 <jomijoseph087@gmail.com>
2026-06-11 11:42:41 +0530

    feat: add testing setup with Jest and Supertest
    - Updated package.json to include Jest and Supertest as devDependencies and added a test script.
    
    - Modified app.js to use ES module syntax for importing express.
    
    - Updated server.js to correctly import app.js with the .js extension.
    
    - Created a new test file (test.js) for testing the POST /tasks endpoint, including a test case for task creation.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
+import express from 'express'
 export const app = express()
-const port = 3000
 
 const tasks = []
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-import express from 'express'
-import { app } from './app'
+import { app } from './app.js'
+
+const port = 3000
+
 app.listen(port, () => console.log(`server started running in port ${port}`))
